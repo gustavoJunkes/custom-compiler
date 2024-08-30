2f919f80e1470b9f99add820afb0950d2f97ea99
Gustavo Junkes <87446204+gustavoJunkes@users.noreply.github.com>
2024-08-30 00:05:25 +0000

    Adding gals files

---
+++ compiler-backend/src/gals/Constants.java
@@ -0,0 +1,45 @@
+public interface Constants extends ScannerConstants
+{
+    int EPSILON  = 0;
+    int DOLLAR   = 1;
+
+    int t_palavra_reservada = 2;
+    int t_main = 3;
+    int t_end = 4;
+    int t_if = 5;
+    int t_elif = 6;
+    int t_else = 7;
+    int t_false = 8;
+    int t_true = 9;
+    int t_read = 10;
+    int t_write = 11;
+    int t_writeln = 12;
+    int t_until = 13;
+    int t_while = 14;
+    int t_TOKEN_15 = 15; //":"
+    int t_TOKEN_16 = 16; //"&&"
+    int t_TOKEN_17 = 17; //"||"
+    int t_TOKEN_18 = 18; //"!"
+    int t_TOKEN_19 = 19; //"=="
+    int t_TOKEN_20 = 20; //"!="
+    int t_TOKEN_21 = 21; //"<"
+    int t_TOKEN_22 = 22; //">"
+    int t_TOKEN_23 = 23; //"+"
+    int t_TOKEN_24 = 24; //"-"
+    int t_TOKEN_25 = 25; //"*"
+    int t_TOKEN_26 = 26; //"/"
+    int t_TOKEN_27 = 27; //","
+    int t_TOKEN_28 = 28; //";"
+    int t_TOKEN_29 = 29; //"="
+    int t_TOKEN_30 = 30; //"("
+    int t_TOKEN_31 = 31; //")"
+    int t_reservada = 32;
+    int t_id = 33;
+    int t_constInt = 34;
+    int t_constFloat = 35;
+    int t_constString = 36;
+    int t_min = 37;
+    int t_mai = 38;
+    int t_letra = 39;
+
+}
