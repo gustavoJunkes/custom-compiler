04620b5328a632952523c2ac84dbe9c6ebf1e62e
borbinhaa <gabrieldeborba07@gmail.com>
2024-09-15 14:01:58 -0300

    Add success compilation

--- compiler-backend/src/Main.java
+++ compiler-backend/src/Main.java
@@ -1,53 +1,71 @@
-import gals.LexicalError;
-import gals.Lexico;
-import gals.Token;
+import java.util.Formatter;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-
-        final String content = "i_9 st  \n linha2 s_teste = \"Gustavo\" \n outra linha";
-
-         execute(content);
+        final String content = args[0].replace("\\n", "\n");
+        execute(content);
     }
 
-    /**
-     *
-     * */
     public static void execute(String content) {
-    Lexico lexico = new Lexico();
+        final Formatter formatter = new Formatter();
+        formatter.format("%-9s%-22s%-10s", "linha", "classe", "lexema");
+
+        Lexico lexico = new Lexico();
         lexico.setInput(content);
         try {
             Token t = null;
-            while ( (t = lexico.nextToken()) != null ) {
-                System.out.println(t.getLexeme()); 
-                
-                // só escreve o lexema, necessário escrever t.getId, t.getPosition()
-            
-                // t.getId () - retorna o identificador da classe. Olhar Constants.java e adaptar, pois 
-            // deve ser apresentada a classe por extenso
-            // t.getPosition () - retorna a posição inicial do lexema no editor, necessário adaptar 
-            // para mostrar a linha	
-
-                // esse código apresenta os tokens enquanto não ocorrer erro
-                // no entanto, os tokens devem ser apresentados SÓ se não ocorrer erro, necessário adaptar 
-            // para atender o que foi solicitado		   
+            while ((t = lexico.nextToken()) != null) {
+                final String clazz = getClassById(t.getId());
+                final int line = findLineByPosition(content, t.getPosition());
+                final String lexeme = clazz.equals("constante_float") ? t.getLexeme().replace(",", ".") : t.getLexeme();
+                formatter.format("%n%-9d%-22s%-10s", line, clazz, lexeme);
             }
-        }
-        catch ( LexicalError e ) {  // tratamento de erros
+            formatter.format("%n%nPrograma compilado com sucesso");
 
+            System.out.println(formatter);
+        } catch (LexicalError e) {  // tratamento de erros
             final String sequence = getSequenceByPosition(content, e);
 
             final String errorMessage = "linha " + findLineByPosition(content, e.getPosition()) + ": " + sequence + " " + e.getMessage();
 
             System.out.println(errorMessage);
             
-            // e.getMessage() - retorna a mensagem de erro de SCANNER_ERRO (olhar ScannerConstants.java 
+            // e.getMessage() - retorna a mensagem de erro de SCANNER_ERRO (olhar ScannerConstants.java
             // e adaptar conforme o enunciado da parte 2)
-            // e.getPosition() - retorna a posição inicial do erro, tem que adaptar para mostrar a 
-            // linha  
-        } 
-	}
+            // e.getPosition() - retorna a posição inicial do erro, tem que adaptar para mostrar a
+            // linha
+        } finally {
+            formatter.close();
+        }
+    }
+
+    private static String getClassById(int id) throws LexicalError {
+        if (id >= 3 && id <= 15) {
+            return "palavra reservada";
+        }
+
+        if (id >= 15 && id <= 31) {
+            return "simbolo especial";
+        }
+
+        if (id == 32) {
+            return "identificador";
+        }
+
+        if (id == 33) {
+            return "constante_float";
+        }
+
+        if (id == 34) {
+            return "constante_int";
+        }
+
+        if (id == 35) {
+            return "constante_string";
+        }
+
+        throw new LexicalError("Id error");
+    }
 
     private static String getSequenceByPosition(String content, LexicalError exception) {
 
@@ -56,7 +74,7 @@ public class Main {
         } else if (exception.getMessage().equalsIgnoreCase("identificador inválido")) {
             // aqui encontrar o identificador completo a partir da posição
             final int endIndex = content.indexOf(" ", exception.getPosition());
-            return content.substring(exception.getPosition()-1, endIndex);
+            return content.substring(exception.getPosition() - 1, endIndex);
         } else if (exception.getMessage().equalsIgnoreCase("constante_string inválida")) {
             return ""; // não precisa mostrar simbolo pra constante string invalida, nem pra comentario de bloco
         }
@@ -67,7 +85,8 @@ public class Main {
 
     /**
      * Given a string and a position, returns the line in which this position is located.
-     * @param content the file content.
+     *
+     * @param content  the file content.
      * @param position the position to find the line.
      * @return the line of the position.
      */
